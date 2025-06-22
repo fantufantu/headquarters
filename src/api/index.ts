63bafd85c3cd1e4bd4f4c66147dd771495855ddf
murukal <tutu@fantufantu.com>
2025-06-22 15:20:02 +0800

    fix: 修复 ApolloClient 配置中的格式问题，统一代码风格

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,42 +1,43 @@
-import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client'
-import { onError } from '@apollo/client/link/error'
-import { Notification } from 'musae'
-import { useAuthentication } from '../store/authentication'
+import { ApolloClient, from, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { Notification } from "musae";
+import { useAuthentication } from "../store/authentication";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
 
   defaultOptions: {
     watchQuery: {
-      fetchPolicy: 'network-only'
-    }
+      fetchPolicy: "network-only",
+    },
   },
 
   link: from([
     onError(({ graphQLErrors, networkError }) => {
-      const errorMessage = graphQLErrors?.[0].message ?? networkError?.message
-      if (!errorMessage) return
+      const errorMessage = graphQLErrors?.[0].message ?? networkError?.message;
+      if (!errorMessage) return;
 
       Notification.error({
-        title: '接口调用异常！',
-        description: errorMessage
-      })
+        title: "接口调用异常！",
+        description: errorMessage,
+      });
     }),
     new HttpLink({
-      uri: '/graphql',
+      uri: "https://api.fantufantu.com",
       fetch: (uri, options) => {
-        const _authenticated = useAuthentication.state.authenticated
-        const _headers = new Headers(options?.headers)
+        const _authenticated = useAuthentication.state.authenticated;
+        const _headers = new Headers(options?.headers);
 
-        _authenticated && _headers.append('Authorization', `Bearer ${_authenticated}`)
+        _authenticated &&
+          _headers.append("Authorization", `Bearer ${_authenticated}`);
 
         return fetch(uri, {
           ...options,
-          headers: Array.from(_headers.entries())
-        })
-      }
-    })
-  ])
-})
+          headers: Array.from(_headers.entries()),
+        });
+      },
+    }),
+  ]),
+});
 
-export { client }
+export { client };
