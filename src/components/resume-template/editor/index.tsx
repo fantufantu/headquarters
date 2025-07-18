c106afdf4f5258b718278203b8c4e0888d391d2b
murukal <tutu@fantufantu.com>
2025-07-18 18:10:32 +0800

    refactor(cv): 模板数据更新

--- src/components/resume-template/editor/index.tsx
+++ src/components/resume-template/editor/index.tsx
@@ -1,81 +1,79 @@
-import { useBoolean } from '@aiszlab/relax'
-import { useLazyQuery, useMutation } from '@apollo/client'
-import { Dialog, Form, Input } from 'musae'
-import { forwardRef, useImperativeHandle, useState } from 'react'
-import { CREATE_RESUME_TEMPLATE, RESUME_TEMPLATE, UPDATE_RESUME_TEMPLATE } from '../../../api/resume-template'
+import { useBoolean } from "@aiszlab/relax";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { Dialog, Form, Input } from "musae";
+import { forwardRef, useImperativeHandle, useState } from "react";
+import { CREATE_RESUME_TEMPLATE, RESUME_TEMPLATE, UPDATE_RESUME_TEMPLATE } from "../../../api/resume-template";
 
 export interface EditorRef {
-  open: (id?: number) => Promise<void>
+  open: (id?: number) => Promise<void>;
 }
 
 interface FormValue {
-  name: string
-  cover: string
+  name: string;
+  cover: string;
 }
 
 const Editor = forwardRef<EditorRef>((_, ref) => {
-  const [isVisible, { turnOn, turnOff }] = useBoolean(false)
-  const [id, setId] = useState<number>()
-  const form = Form.useForm<FormValue>()
-  const [queryResumeTemplate] = useLazyQuery(RESUME_TEMPLATE)
-  const [createResumeTemplate] = useMutation(CREATE_RESUME_TEMPLATE)
-  const [updateResumeTemplate] = useMutation(UPDATE_RESUME_TEMPLATE)
+  const [isVisible, { turnOn, turnOff }] = useBoolean(false);
+  const [id, setId] = useState<number>();
+  const form = Form.useForm<FormValue>();
+  const [queryResumeTemplate] = useLazyQuery(RESUME_TEMPLATE);
+  const [createResumeTemplate] = useMutation(CREATE_RESUME_TEMPLATE);
+  const [updateResumeTemplate] = useMutation(UPDATE_RESUME_TEMPLATE);
 
   useImperativeHandle(ref, () => {
     return {
       open: async (id) => {
-        form.clear()
+        form.clear();
 
         if (id) {
           const _resumeTemplate = (await queryResumeTemplate({ variables: { id } }).catch(() => null))?.data
-            ?.resumeTemplate
-          if (!_resumeTemplate) return
-
-          console.log('_resumeTemplate====', _resumeTemplate)
+            ?.resumeTemplate;
+          if (!_resumeTemplate) return;
 
           form.setFieldsValue({
             name: _resumeTemplate.name,
-            cover: _resumeTemplate.cover
-          })
+            cover: _resumeTemplate.cover,
+          });
         }
 
-        setId(id)
-        turnOn()
-      }
-    }
-  })
+        setId(id);
+        turnOn();
+      },
+    };
+  });
 
   const submit = async () => {
-    const isValid = await form.validate().catch(() => false)
-    if (!isValid) return
+    const isValid = await form.validate().catch(() => false);
+    if (!isValid) return;
 
-    const _values = form.getFieldsValue() as FormValue
+    const _values = form.getFieldsValue() as FormValue;
     const isSucceed = await (id
       ? updateResumeTemplate({ variables: { id, input: _values } })
       : createResumeTemplate({
           variables: {
-            input: _values
-          }
+            input: _values,
+          },
         }).then((_created) => !!_created.data?.createResumeTemplate)
-    ).catch(() => false)
+    ).catch(() => false);
 
-    if (!isSucceed) return
-    turnOff()
-  }
+    if (!isSucceed) return;
+    turnOff();
+  };
 
   return (
-    <Dialog title='编辑' open={isVisible} onClose={turnOff} onConfirm={submit}>
+    <Dialog title="编辑" open={isVisible} onClose={turnOff} onConfirm={submit}>
       <Form form={form}>
-        <Form.Item label='模板名称' name='name' required>
-          <Input placeholder='请输入模板名称' />
+        <Form.Item label="模板名称" name="name" required>
+          <Input placeholder="请输入模板名称" />
         </Form.Item>
 
-        <Form.Item label='模板封面' name='cover' required>
-          <Input placeholder='请输入模板封面' />
+        <Form.Item label="模板封面" name="cover" required>
+          <Input placeholder="请输入模板封面" />
         </Form.Item>
       </Form>
     </Dialog>
-  )
-})
+  );
+});
 
-export default Editor
+export default Editor;
