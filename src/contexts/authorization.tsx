ddb557c19b085d0279d8d56e443db95426477062
murukal <tutu@fantufantu.com>
2025-12-16 09:33:45 +0800

    feat(authorization): integrate AuthorizationProvider into Layout component

--- src/contexts/authorization.tsx
+++ src/contexts/authorization.tsx
@@ -1,13 +1,35 @@
-import { createContext } from "react";
+import { useQuery } from "@apollo/client/react";
+import { createContext, ReactNode, useContext } from "react";
+import { AUTHORIZED } from "../api/authorization";
+import { Skeleton } from "musae";
+import { Authorization } from "../api/authorization.types";
 
-const AuthorizationContext = createContext({});
+interface ContextValue {
+  authorizations?: Authorization[];
+}
 
-const AuthorizationProvider = () => {
+const AuthorizationContext = createContext<ContextValue>({});
+
+interface Props {
+  children: ReactNode;
+}
+
+const AuthorizationProvider = ({ children }: Props) => {
+  // 初始化用户权限
+  const { data, loading } = useQuery(AUTHORIZED);
+
+  if (loading) {
+    return <Skeleton />;
+  }
+
   return (
-    <AuthorizationContext.Provider value={{}}>
-      <></>
+    <AuthorizationContext.Provider value={{ authorizations: data?.authorized }}>
+      {children}
     </AuthorizationContext.Provider>
   );
 };
 
+const useAuthorization = () => useContext(AuthorizationContext);
+
 export default AuthorizationContext;
+export { useAuthorization, AuthorizationProvider };
--- src/layouts/layout/index.tsx
+++ src/layouts/layout/index.tsx
@@ -6,6 +6,7 @@ import { LOGOUT } from "../../api/authentication";
 import { createElement, useCallback } from "react";
 import { useAuthentication } from "../../store/authentication";
 import { WbSunny, Bedtime } from "musae/icons";
+import { AuthorizationProvider } from "../../contexts/authorization";
 
 const Layout = () => {
   const navigations = useNavigations();
@@ -25,54 +26,56 @@ const Layout = () => {
   }, [navigate]);
 
   return (
-    <Bench
-      title="水番二土"
-      layout="side"
-      location={pathname.pathname}
-      navigations={navigations}
-      onNavigate={navigate}
-      classNames={{
-        main: "px-10 pb-8",
-      }}
-      trailing={
-        <div className="flex gap-4">
-          <IconButton onClick={toggle} variant="text" size="small">
-            {createElement(mode === "light" ? Bedtime : WbSunny)}
-          </IconButton>
-          <Popover
-            content={
-              <div className="flex flex-col items-center gap-2">
-                <span>你好，{me?.nickname ?? me?.username}</span>
-                <Menu
-                  items={[
-                    {
-                      label: "修改个人信息",
-                      key: "edit",
-                      onClick: toSetting,
-                      className: "justify-center",
-                    },
-                    {
-                      label: "退出登录",
-                      key: "logout",
-                      onClick: onLogout,
-                      className: "justify-center",
-                    },
-                  ]}
-                />
-              </div>
-            }
-          >
-            <Avatar
-              src={me?.avatar}
-              crossOrigin="anonymous"
-              referrerPolicy="strict-origin-when-cross-origin"
-            />
-          </Popover>
-        </div>
-      }
-    >
-      <Outlet />
-    </Bench>
+    <AuthorizationProvider>
+      <Bench
+        title="水番二土"
+        layout="side"
+        location={pathname.pathname}
+        navigations={navigations}
+        onNavigate={navigate}
+        classNames={{
+          main: "px-10 pb-8",
+        }}
+        trailing={
+          <div className="flex gap-4">
+            <IconButton onClick={toggle} variant="text" size="small">
+              {createElement(mode === "light" ? Bedtime : WbSunny)}
+            </IconButton>
+            <Popover
+              content={
+                <div className="flex flex-col items-center gap-2">
+                  <span>你好，{me?.nickname ?? me?.username}</span>
+                  <Menu
+                    items={[
+                      {
+                        label: "修改个人信息",
+                        key: "edit",
+                        onClick: toSetting,
+                        className: "justify-center",
+                      },
+                      {
+                        label: "退出登录",
+                        key: "logout",
+                        onClick: onLogout,
+                        className: "justify-center",
+                      },
+                    ]}
+                  />
+                </div>
+              }
+            >
+              <Avatar
+                src={me?.avatar}
+                crossOrigin="anonymous"
+                referrerPolicy="strict-origin-when-cross-origin"
+              />
+            </Popover>
+          </div>
+        }
+      >
+        <Outlet />
+      </Bench>
+    </AuthorizationProvider>
   );
 };
 
